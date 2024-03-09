e7a14d5165c07a600e825551407e60c43f5e1b02
Vlad Gershun <dev.vladgershun@gmail.com>
2024-03-09 12:59:01 -0600

    Initial Commit

---
+++ StockWise/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StockWise
+//
+//  Created by Vlad Gershun on 3/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StockWise/StockWiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockWiseApp.swift
+//  StockWise
+//
+//  Created by Vlad Gershun on 3/9/24.
+//
+
+import SwiftUI
+
+@main
+struct StockWiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
